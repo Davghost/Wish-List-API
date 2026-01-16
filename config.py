19a20ea975028a779aca8545471a8f3a047ce893
davi <acrookedfork@gmail.com>
2026-01-16 20:13:31 -0300

    Change config from db

--- config.py
+++ config.py
@@ -9,7 +9,11 @@ class Config:
    SECRET_KEY = os.environ.get("SECRET_KEY")
    APP_TITLE = "Wish List API"
 
-   SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+   SQLALCHEMY_DATABASE_URI = os.environ.get(
+      "DATABASE_URL",
+      "sqlite:///" + os.path.join(basedir, "app.db")
+   )   
+   
    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
